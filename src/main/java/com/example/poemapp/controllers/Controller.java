039f4b9e301c85e235e700ebf469b351a50e557d
Perom Peter Pen <officialpeterp@yahoo.com>
2022-08-05 09:37:07 -0700

    adds controller, welcome demo

---
+++ src/main/java/com/example/poemapp/controllers/Controller.java
@@ -0,0 +1,13 @@
+package com.example.poemapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping(value = "/")
+    public String getPage() {
+        return "Welcome";
+    }
+}
