dcfb3c5467e770abf51754fbd05637b42375f0e2
Perom Peter Pen <officialpeterp@yahoo.com>
2022-08-08 13:55:29 -0700

    updates controllers

--- src/main/java/com/example/poemapp/controllers/Controller.java
+++ src/main/java/com/example/poemapp/controllers/Controller.java
@@ -1,13 +1,26 @@
 package com.example.poemapp.controllers;
 
+import com.example.poemapp.models.Poem;
+import com.example.poemapp.models.repo.PoemRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class Controller {
 
+    @Autowired
+    private PoemRepo poemRepo;
+
     @GetMapping(value = "/")
     public String getPage() {
         return "Welcome";
+    }
+
+    @GetMapping(value = "/poems")
+    public List<Poem> getPoems() {
+        return poemRepo.findAll();
     }
 }
