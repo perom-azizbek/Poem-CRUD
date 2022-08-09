ca2921a8c08956df7a7fcd58f7c994487bc45694
Perom Peter Pen <officialpeterp@yahoo.com>
2022-08-09 12:39:14 -0700

    adds create of crud

--- src/main/java/com/example/poemapp/controllers/Controller.java
+++ src/main/java/com/example/poemapp/controllers/Controller.java
@@ -2,9 +2,9 @@ package com.example.poemapp.controllers;
 
 import com.example.poemapp.models.Poem;
 import com.example.poemapp.models.repo.PoemRepo;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,13 +14,22 @@ public class Controller {
     @Autowired
     private PoemRepo poemRepo;
 
+    //Welcome demo
     @GetMapping(value = "/")
     public String getPage() {
         return "Welcome";
     }
 
+    //READS(GET) array of data
     @GetMapping(value = "/poems")
     public List<Poem> getPoems() {
         return poemRepo.findAll();
+    }
+
+    //CREATES(POST) array of data
+    @PostMapping(value = "/save")
+    public String savePoem(@RequestBody Poem poem) {
+        poemRepo.save(poem);
+        return "Saving!";
     }
 }
